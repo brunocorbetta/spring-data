251a049db5c1e2a294cd050b87ea361b89b496e3
bruno corbetta <corbetta510@gmail.com>
2021-08-15 12:12:00 -0300

    Initial commit

---
+++ src/main/java/br/com/alura/springdata/repository/FuncionarioRepository.java
@@ -0,0 +1,27 @@
+package br.com.alura.springdata.repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.alura.springdata.orm.Funcionario;
+import br.com.alura.springdata.orm.FuncionarioProjecao;
+
+@Repository
+public interface FuncionarioRepository extends PagingAndSortingRepository<Funcionario, Integer> {
+	List<Funcionario> findByNome(String nome); //pelo spring direto
+
+	@Query("SELECT f FROM Funcionario f WHERE f.nome = :nome "
+			+ "AND f.salario >= :salario AND f.dataContratacao = :data") // usando sql mas com o nome das classes em java 
+	List<Funcionario> findNomeSalarioMaiorDataContratacao(String nome, Double salario, LocalDate data);
+
+	@Query(value = "SELECT * FROM funcionarios f WHERE f.data_contratacao >= :data", nativeQuery = true) //usando o sql
+	List<Funcionario> findDataContratacaoMaior(LocalDate data);
+	
+	//consulta com projecao, tem que criar uma interface com os atributos do resquest no banco.
+	@Query(value = "SELECT f.id, f.nome, f.salario FROM funcionarios f", nativeQuery = true)
+	List<FuncionarioProjecao> findFuncionarioSalario();
+}
